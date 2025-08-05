b00d608fff887fcc32bbb6652e6705fec55ff0a2
Kelvin Crabb <kelvin.crabb@icloud.com>
2025-08-05 15:42:58 +0100

    Configured vite to bundle images from assets directory, updated gitignore to ignore vite build assets

--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -10,7 +10,11 @@
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300&display=swap" rel="stylesheet">
 </head>
-<body>
-
+<body class="bg-light">
+    <div class="container p-3">
+        <div class="row">
+            <div class="col"><img src="{{ Vite::asset('assets/logo.png') }}" /></div>
+        </div>
+    </div>
 </body>
 </html>
